8963cc6b33ac682c3cbf6003a6feefd757873da4
Maarten Engels <maartene@thedreamweb.eu>
2023-12-04 10:48:03 +0100

    Day 4 done

--- day4/Sources/day4/day4.swift
+++ day4/Sources/day4/day4.swift
@@ -1,2 +1,83 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
+import Foundation
+
+struct Card {
+    let id: Int
+    let winningNumbers: [Int]
+    let ownNumbers: [Int]
+    
+    init(cardString: String) {
+        let descriptionContentSplit = cardString.split(separator: ":")
+            .map { String($0) }
+        
+        guard descriptionContentSplit.count == 2 else {
+            fatalError("There should be two splits")
+        }
+        
+        id = Int(descriptionContentSplit[0].trimmingCharacters(in: .letters)
+            .trimmingCharacters(in: .whitespaces))!
+        
+        let winningAndOwnNumbers = descriptionContentSplit[1].split(separator: "|")
+            .map { String($0) }
+        
+        winningNumbers = winningAndOwnNumbers[0].split(separator: " ")
+            .map { String($0) }
+            .map { Int($0)! }
+        
+        ownNumbers = winningAndOwnNumbers[1].split(separator: " ")
+            .map { String($0) }
+            .map { Int($0)! }
+    }
+    
+    var matchingNumberCount: Int {
+        let winningSet = Set(winningNumbers)
+        let ownSet = Set(ownNumbers)
+        let overlap = winningSet.intersection(ownSet)
+        return overlap.count
+    }
+    
+    var score: Int {
+        let subscore = (0 ..< matchingNumberCount).reduce(1) { result, _ in result * 2 }
+        return subscore / 2
+    }
+}
+
+// MARK: Part 1
+func calculatePointsForCard(_ cardString: String) -> Int {
+    Card(cardString: cardString).score
+}
+
+func calculateTotalPoints(_ input: String) -> Int {
+    let calculatedPoints = input.split(separator: "\n")
+        .map { String($0) }
+        .map { calculatePointsForCard($0) }
+    
+    return calculatedPoints.reduce(0, +)
+}
+
+// MARK: Part 2
+func calculateTotalNumberOfScratchCards(_ input: String) -> Int {
+    var cards = input.split(separator: "\n")
+        .map { String($0) }
+        .map { Card(cardString: $0) }
+    
+    let maxCardID = cards.max(by: { $0.id < $1.id })!.id
+    
+    var cardCopyDictionary = [Int: Card]()
+    for card in cards {
+        cardCopyDictionary[card.id] = card
+    }
+    
+    for cardID in 0 ..< maxCardID {
+        let cardsToCopy = cards.filter { $0.id == cardID }
+        for card in cardsToCopy {
+            for di in 0 ..< card.matchingNumberCount {
+                if let copy = cardCopyDictionary[cardID + di + 1] {
+                    cards.append(copy)
+                }
+            }
+        }
+        print(cards.count, cardID)
+    }
+    
+    return cards.count
+}
--- day4/Tests/day4Tests/day4Tests.swift
+++ day4/Tests/day4Tests/day4Tests.swift
@@ -2,11 +2,67 @@ import XCTest
 @testable import day4
 
 final class day4Tests: XCTestCase {
-    func testExample() throws {
-        // XCTest Documentation
-        // https://developer.apple.com/documentation/xctest
-
-        // Defining Test Cases and Test Methods
-        // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
+    let exampleInput =
+    """
+    Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+    Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+    Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+    Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+    Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+    Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11
+    """
+    
+    func test_createCard() {
+        let card = Card(cardString: "Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53")
+        XCTAssertEqual(card.winningNumbers, [41, 48, 83, 86, 17])
+        XCTAssertEqual(card.ownNumbers, [83, 86, 6, 31, 17, 9, 48, 53])
+    }
+    
+    // MARK: Part 1
+    func test_calculatePoints_withExampleInput() {
+        let expectedPoints = [8, 2, 2, 1, 0, 0]
+        
+        let calculatedPoints = exampleInput.split(separator: "\n")
+            .map { String($0) }
+            .map { calculatePointsForCard($0) }
+        
+        XCTAssertEqual(calculatedPoints, expectedPoints)
+    }
+    
+    func test_calculateTotalPoints_withExampleInput() {
+        XCTAssertEqual(calculateTotalPoints(exampleInput), 13)
+    }
+    
+    func test_part1() {
+        let points = calculateTotalPoints(input)
+        XCTAssertEqual(points, 25231)
+    }
+    
+    // MARK: Part 2
+    func test_createCard_id() {
+        let card = Card(cardString: "Card 42:  1 21 53 59 44 | 69 82 63 72 16 21 14  1")
+        XCTAssertEqual(card.id, 42)
+    }
+    
+    func test_matchingNumberCount_withExampleInput() {
+        let expectedCount = [4, 2, 2, 1, 0, 0]
+        
+        let cards = exampleInput.split(separator: "\n")
+            .map { String($0) }
+            .map { Card(cardString: $0) }
+        
+        for i in 0 ..< cards.count {
+            XCTAssertEqual(cards[i].matchingNumberCount, expectedCount[i])
+        }
+    }
+    
+    func test_calculateTotalNumberOfScratchCards_withExampleInput() {
+        XCTAssertEqual(calculateTotalNumberOfScratchCards(exampleInput), 30)
+    }
+    
+    // WARNING: This test runs about 2 minutes!!!
+    func test_part2() {
+        let calculatedTotal = calculateTotalNumberOfScratchCards(input)
+        XCTAssertEqual(calculatedTotal, 9721255)
     }
 }
