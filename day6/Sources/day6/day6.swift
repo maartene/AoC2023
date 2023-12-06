c4800a46c2efcf382f2dc54b513809a26ba8deac
Maarten Engels <maartene@mac.com>
2023-12-06 07:34:12 +0000

    day 6 complete

--- day6/Sources/day6/day6.swift
+++ day6/Sources/day6/day6.swift
@@ -1,2 +1,61 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
+import Foundation
+
+struct Race {
+    let time: Int
+    let recordDistance: Int
+
+    func distanceForButtonHeldDownTime(_ heldDownTime: Int) -> Int {
+        let remainingRaceTime = time - heldDownTime
+        return remainingRaceTime * heldDownTime
+    }
+
+    var winningOptions: [Int] {
+        (0 ..< time)
+        .filter { distanceForButtonHeldDownTime($0) > recordDistance }
+    }
+
+    var winningOptionsCount: Int {
+        winningOptions.count
+    }
+}
+
+func interpretInput_part1(_ input: String) -> [Race] {
+    let lines = input.split(separator: "\n").map { String($0) }
+    let raceTimes = lines[0]
+        .split(separator: " ")
+        .map { String($0) }
+        .compactMap { Int($0)} 
+    let raceDistances = lines[1]
+        .split(separator: " ")
+        .map { String($0) }
+        .compactMap { Int($0)} 
+    
+    var races = [Race]()
+    for i in 0 ..< raceTimes.count {
+      races.append(Race(time: raceTimes[i], recordDistance: raceDistances[i]))
+    }
+
+    return races
+}
+
+func winningOptionsCountForRaces(_ input: String) -> [Int] {
+    let races = interpretInput_part1(input)
+
+    return races.map { $0.winningOptionsCount }
+}
+
+func numberOfWaysToBeatRecord(_ input: String) -> Int {
+    let optionsCount = winningOptionsCountForRaces(input)
+    return optionsCount.reduce(1, *)
+}
+
+func interpretInput_part2(_ input: String) -> Race {
+    let lines = input.split(separator: "\n").map {String($0) }
+    let numbers = lines
+    .map {
+        $0.filter { $0.isNumber }
+    }
+    .compactMap { Int($0) }
+    
+    return Race(time: numbers[0], recordDistance: numbers[1])
+}
--- day6/Tests/day6Tests/day6Tests.swift
+++ day6/Tests/day6Tests/day6Tests.swift
@@ -2,11 +2,52 @@ import XCTest
 @testable import day6
 
 final class day6Tests: XCTestCase {
-    func testExample() throws {
-        // XCTest Documentation
-        // https://developer.apple.com/documentation/xctest
+let exampleInput = 
+    """
+    Time:      7  15   30
+    Distance:  9  40  200
+    """
 
-        // Defining Test Cases and Test Methods
-        // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
+    func test_winningOptions_withSingleExampleInput() {
+        let race = Race(time: 7, recordDistance: 9)
+        XCTAssertEqual(race.winningOptions, [2,3,4,5])
+    }
+
+    func test_winningOptionsCountForRaces() {
+        let expected = [4, 8, 9]
+        let result = winningOptionsCountForRaces(exampleInput)
+        XCTAssertEqual(expected.count, result.count)
+        for i in 0 ..< expected.count {
+            XCTAssertEqual(result[i], expected[i])
+        }
+    }
+
+    func test_numberOfWaysToBeatRecord_withExampleInput() {
+        XCTAssertEqual(numberOfWaysToBeatRecord(exampleInput), 288)
+    }
+
+    func test_part1() {
+        let result = numberOfWaysToBeatRecord(input)
+        XCTAssertEqual(result, 449550)
+    }
+
+
+    // MARK: Part 2
+    func test_test_winningOptions_withLargerInput() {
+        let race = Race(time: 71530, recordDistance: 940200)
+        XCTAssertEqual(race.winningOptionsCount, 71503)
+    }
+
+    func test_interpretInput_part2_withExampleInput() {
+        let race = interpretInput_part2(exampleInput)
+        XCTAssertEqual(race.time, 71530)
+        XCTAssertEqual(race.recordDistance, 940200)
+    }
+
+    func test_part2() {
+        let race = interpretInput_part2(input)
+        print("Race: \(race)")
+        let result = race.winningOptionsCount
+        XCTAssertEqual(result, 28360140)
     }
 }
