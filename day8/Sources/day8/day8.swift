f7a8bad648407ba05cca1f80f620c498b717a1b3
Maarten Engels <maartene@thedreamweb.eu>
2023-12-08 14:12:53 +0100

    Day 8 done

--- day8/Sources/day8/day8.swift
+++ day8/Sources/day8/day8.swift
@@ -1,2 +1,81 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
+import Foundation
+
+struct StepEntry {
+    let left: String
+    let right: String
+    
+    init(_ input: String) {
+        let words = input.split(separator: " ").map { String($0) }
+        left = words[0].filter { $0.isLetter || $0.isNumber }
+        right = words[1].filter { $0.isLetter || $0.isNumber }
+    }
+}
+
+func createSequenceAndStepEntries(_ input: String) -> (sequence: [Character], stepEnties: [String: StepEntry]) {
+    let lines = input.split(separator: "\n").map { String($0) }
+    let sequence = lines[0].map { $0 }
+    
+    var stepEntries = [String: StepEntry]()
+    for line in lines.dropFirst() {
+        let words = line.split(separator: "=").map { String($0) }
+        stepEntries[words[0].filter({ $0.isLetter || $0.isNumber } )] = StepEntry(words[1])
+    }
+    
+    return (sequence, stepEntries)
+}
+
+func stepsToReachZZZ(_ input: String, for startingStep: String = "AAA", searchForEndZOnly: Bool = false) -> Int {
+    let parsedInput = createSequenceAndStepEntries(input)
+    let sequence = parsedInput.sequence
+    let stepEntries = parsedInput.stepEnties
+        
+    let endStates = searchForEndZOnly ? stepEntries.filter({ $0.key.last! == "Z" }).keys.map({ $0 as! String }) : ["ZZZ"]
+    
+    var sequenceIndex = 0
+    var currentStepKey = startingStep
+    while endStates.contains(currentStepKey) == false {
+        let currentStep = stepEntries[currentStepKey]!
+        let next = sequence[sequenceIndex % sequence.count] == "R" ? currentStep.right : currentStep.left
+        currentStepKey = next
+        sequenceIndex += 1
+    }
+    
+    return sequenceIndex
+    
+}
+
+// MARK: Part 2
+func ghostly_stepsToReachZZZ(_ input: String) -> Int {
+    func gcd(_ num1: Int, _ num2: Int) -> Int {
+        var a = num1
+        var b = num2
+        while b != 0 {
+            let t = b
+            b = a % b
+            a = t
+        }
+        return a
+    }
+    
+    func lcm(of numbers: [Int]) -> Int {
+        var result = numbers.first ?? 1
+        for number in numbers {
+            result = (result * number) / gcd(result, number)
+        }
+        return result
+    }
+    
+    let stepEntries = createSequenceAndStepEntries(input).stepEnties
+    
+    let startingStepEntries = stepEntries.filter { pair in
+        pair.key.last! == "A"
+    }.keys.map { String($0) }
+    
+    //print(startingStepEntries)
+    
+    let numbers = startingStepEntries.map { entry in
+        stepsToReachZZZ(input, for: entry, searchForEndZOnly: true)
+    }
+        
+    return lcm(of: numbers)
+}
--- day8/Tests/day8Tests/day8Tests.swift
+++ day8/Tests/day8Tests/day8Tests.swift
@@ -2,11 +2,79 @@ import XCTest
 @testable import day8
 
 final class day8Tests: XCTestCase {
-    func testExample() throws {
-        // XCTest Documentation
-        // https://developer.apple.com/documentation/xctest
+    let exampleInput =
+    """
+    RL
 
-        // Defining Test Cases and Test Methods
-        // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
+    AAA = (BBB, CCC)
+    BBB = (DDD, EEE)
+    CCC = (ZZZ, GGG)
+    DDD = (DDD, DDD)
+    EEE = (EEE, EEE)
+    GGG = (GGG, GGG)
+    ZZZ = (ZZZ, ZZZ)
+    """
+    
+    let exampleInput2 =
+    """
+    LLR
+
+    AAA = (BBB, BBB)
+    BBB = (AAA, ZZZ)
+    ZZZ = (ZZZ, ZZZ)
+    """
+    
+    func test_createStepEntry() {
+        let input = " (BBB, CCC)"
+        
+        let stepEntry = StepEntry(input)
+        
+        XCTAssertEqual(stepEntry.left, "BBB")
+        XCTAssertEqual(stepEntry.right, "CCC")
+    }
+        
+    func test_stepsToReachZZZ_withExampleInput() {
+        let result = stepsToReachZZZ(exampleInput)
+        
+        XCTAssertEqual(result, 2)
+    }
+    
+    func test_stepsToReachZZZ_withExampleInput2() {
+        let result = stepsToReachZZZ(exampleInput2)
+        
+        XCTAssertEqual(result, 6)
+    }
+    
+    func test_part1() {
+        let result = stepsToReachZZZ(input)
+        XCTAssertEqual(result, 11567)
+    }
+    
+    // MARK: Part 2
+    
+    let exampleInput_part2 =
+    """
+    LR
+
+    11A = (11B, XXX)
+    11B = (XXX, 11Z)
+    11Z = (11B, XXX)
+    22A = (22B, XXX)
+    22B = (22C, 22C)
+    22C = (22Z, 22Z)
+    22Z = (22B, 22B)
+    XXX = (XXX, XXX)
+    """
+    
+    func test_stepsToReachZZZ_withExampleInputPart2() {
+        let result = ghostly_stepsToReachZZZ(exampleInput_part2)
+        
+        XCTAssertEqual(result, 6)
+    }
+    
+    func test_part2() {
+        let result = ghostly_stepsToReachZZZ(input)
+        print(result)
+        XCTAssertEqual(result, 9_858_474_970_153)
     }
 }
