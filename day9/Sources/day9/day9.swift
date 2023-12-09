13fa31b138c3abced8e66f1a4a73a55e4daf90bf
Maarten Engels <maartene@mac.com>
2023-12-09 09:47:27 +0100

    Day 9 done

--- day9/Sources/day9/day9.swift
+++ day9/Sources/day9/day9.swift
@@ -1,2 +1,82 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
+import Foundation
+
+func differences(_ sequence: [Int]) -> [Int] {
+    var result = [Int]()
+    for i in 0 ..< sequence.count - 1 {
+        result.append(sequence[i + 1] - sequence[i])
+    }
+    return result
+}
+
+func allZeroes(_ sequence: [Int]) -> Bool {
+    sequence.filter { $0 != 0 }.count == 0
+}
+ 
+func predictNext(_ sequence: [Int]) -> Int {
+    var sequences = [sequence]
+    
+    while allZeroes(sequences.last!) == false {
+        sequences.append(differences(sequences.last!))
+    }
+    
+    for i in 0 ..< sequences.count {
+        sequences[i].append(0)
+    }
+        
+    for i in 1 ... sequences.count - 1 {
+        let index = sequences.count - i
+        let sequenceBottom = sequences[index]
+        var sequenceTop = sequences[index - 1]
+        sequenceTop[sequenceTop.count - 1] = sequenceBottom.last! + sequenceTop[sequenceTop.count - 2]
+        
+        sequences[index - 1] = sequenceTop
+    }
+        
+    return sequences[0].last!
+}
+
+func inputToSequences(_ input: String) -> [[Int]] {
+    let lines = input.split(separator: "\n").map { String($0) }
+    return lines.map { line in
+        line.split(separator: " ")
+            .map { String($0) }
+            .compactMap { Int($0) }
+    }
+}
+
+func sumPredictedNumbers(_ input: String) -> Int {
+    let sequences = inputToSequences(input)
+    let predictedNumbers = sequences.map { predictNext($0) }
+    return predictedNumbers.reduce(0, +)
+}
+
+// MARK: Part 2
+func predictPrevious(_ sequence: [Int]) -> Int {
+    var sequences = [sequence]
+    
+    while allZeroes(sequences.last!) == false {
+        sequences.append(differences(sequences.last!))
+    }
+    
+    for i in 0 ..< sequences.count {
+        sequences[i].insert(0, at: 0)
+    }
+        
+    for i in 1 ... sequences.count - 1 {
+        let index = sequences.count - i
+        let sequenceBottom = sequences[index]
+        var sequenceTop = sequences[index - 1]
+        sequenceTop[0] = sequenceTop[1] - sequenceBottom[0]
+        
+        sequences[index - 1] = sequenceTop
+    }
+        
+    return sequences[0][0]
+}
+
+func sumPredictedPreviousNumbers(_ input: String) -> Int {
+    let sequences = inputToSequences(input)
+    let predictedNumbers = sequences.map { predictPrevious($0) }
+    return predictedNumbers.reduce(0, +)
+}
+
--- day9/Tests/day9Tests/day9Tests.swift
+++ day9/Tests/day9Tests/day9Tests.swift
@@ -2,11 +2,85 @@ import XCTest
 @testable import day9
 
 final class day9Tests: XCTestCase {
-    func testExample() throws {
-        // XCTest Documentation
-        // https://developer.apple.com/documentation/xctest
-
-        // Defining Test Cases and Test Methods
-        // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
+    let exampleInput =
+    """
+    0 3 6 9 12 15
+    1 3 6 10 15 21
+    10 13 16 21 30 45
+    """
+    
+    func test_differences_03691215() {
+        let sequence = [0,3,6,9,12,15]
+        let expected = [3,3,3,3,3]
+        
+        let result = differences(sequence)
+        
+        XCTAssertEqual(result, expected)
+    }
+    
+    func test_differences_136101521() {
+        let sequence = [1,3, 6, 10, 15, 21]
+        let expected = [2,3,4,5,6]
+        
+        let result = differences(sequence)
+        
+        XCTAssertEqual(result, expected)
+    }
+    
+    func test_predictNext_03691215() {
+        let sequence = [0,3,6,9,12,15]
+        
+        let result = predictNext(sequence)
+        
+        XCTAssertEqual(result, 18)
+    }
+    
+    func test_predictNext_136101521() {
+        let sequence = [1,3, 6, 10, 15, 21]
+        
+        let result = predictNext(sequence)
+        
+        XCTAssertEqual(result, 28)
+    }
+    
+    func test_predictNext_101316213045() {
+        let sequence = [10, 13, 16, 21, 30, 45]
+        
+        let result = predictNext(sequence)
+        
+        XCTAssertEqual(result, 68)
+    }
+    
+    func test_sumPredictedNumbers_forExampleInput() {
+        let result = sumPredictedNumbers(exampleInput)
+        XCTAssertEqual(result, 114)
+    }
+    
+    func test_part1() {
+        let result = sumPredictedNumbers(input)
+        XCTAssertEqual(result, 1972648895)
+    }
+    
+    // MARK: Part 2
+    func test_predictPrevious_forExampleInput() {
+        let input = [
+            [0,3,6,9,12,15],
+            [1,3, 6, 10, 15, 21],
+            [10, 13, 16, 21, 30, 45]
+        ]
+        
+        let expected = [-3,0,5]
+        let result = input.map { predictPrevious($0) }
+        XCTAssertEqual(result, expected)
+    }
+    
+    func test_sumPredictedPreviousNumbers_forExampleInput() {
+        let result = sumPredictedPreviousNumbers(exampleInput)
+        XCTAssertEqual(result, 2)
+    }
+    
+    func test_part2() {
+        let result = sumPredictedPreviousNumbers(input)
+        XCTAssertEqual(result, 919)
     }
 }
