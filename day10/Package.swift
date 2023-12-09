b1126dd25842dcf3ef546e70dfa77f646da1cf14
Maarten Engels <maartene@mac.com>
2023-12-09 19:33:33 +0100

    Day 10 scaffolding

---
+++ day10/Package.swift
@@ -0,0 +1,23 @@
+// swift-tools-version: 5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "day10",
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "day10",
+            targets: ["day10"]),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "day10"),
+        .testTarget(
+            name: "day10Tests",
+            dependencies: ["day10"]),
+    ]
+)
