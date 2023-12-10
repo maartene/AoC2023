e8dbff0168b9dfa7de36003cb80b20633df43adf
Maarten Engels <maartene@thedreamweb.eu>
2023-12-10 15:07:08 +0100

    Day 10 done

--- day10/Sources/day10/day10.swift
+++ day10/Sources/day10/day10.swift
@@ -1,2 +1,225 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
+import Foundation
+
+struct Map {
+    let startingPosition: Vector2D
+    let nodes: [Vector2D: Character]
+    let colCount: Int
+    let rowCount: Int
+
+    init(_ input: String) {
+        let lines = input.split(separator: "\n").map { String($0) }
+        var nodes = [Vector2D: Character]()
+        for y in 0 ..< lines.count {
+            let line = lines[y].split(separator: "").map { Character(String($0)) }
+            for x in 0 ..< line.count {
+                nodes[Vector2D(x: x, y: y)] = line[x]
+            }
+        }
+        
+        rowCount = lines.count
+        colCount = lines[0].count
+        
+        self.nodes = nodes
+        
+        guard let sPosition = nodes.first(where: { $0.value == "S" })?.key else {
+            fatalError("No S found in input")
+        }
+        
+        startingPosition = sPosition
+    }
+    
+    var startingPositionCharacter: Character {
+        let east = Vector2D(x: startingPosition.x + 1, y: startingPosition.y)
+        let west = Vector2D(x: startingPosition.x - 1, y: startingPosition.y)
+        let north = Vector2D(x: startingPosition.x, y: startingPosition.y - 1)
+        let south = Vector2D(x: startingPosition.x, y: startingPosition.y + 1)
+        
+        var connections = [Vector2D]()
+        if "-J7".contains(nodes[east, default: "."]) {
+            connections.append(east)
+        }
+        
+        if "-LF".contains(nodes[west, default: "."]) {
+            connections.append(west)
+        }
+        
+        if "|7F".contains(nodes[north, default: "."]) {
+            connections.append(north)
+        }
+        
+        if "|LJ".contains(nodes[south, default: "."]) {
+            connections.append(south)
+        }
+        
+        if connections.contains(north) && connections.contains(south) {
+            return "|"
+        } else if connections.contains(east) && connections.contains(west) {
+            return "-"
+        } else if connections.contains(north) && connections.contains(east) {
+            return "L"
+        } else if connections.contains(north) && connections.contains(west) {
+            return "J"
+        } else if connections.contains(south) && connections.contains(west) {
+            return "7"
+        } else if connections.contains(south) && connections.contains(east) {
+            return "F"
+        } else {
+            fatalError("Unable to determine starting position connection")
+        }
+        
+    }
+    
+    func getCharacterAt(_ coord: Vector2D) -> Character {
+        if nodes[coord] == "S" {
+            return startingPositionCharacter
+        }
+        
+        return nodes[coord, default: " "]
+        
+    }
+    
+    func getNeighboursFor(_ node: Vector2D) -> [Vector2D] {
+        let east = Vector2D(x: node.x + 1, y: node.y)
+        let west = Vector2D(x: node.x - 1, y: node.y)
+        let north = Vector2D(x: node.x, y: node.y - 1)
+        let south = Vector2D(x: node.x, y: node.y + 1)
+        
+        switch getCharacterAt(node) {
+        case "|":
+            return [north, south]
+        case "-":
+            return [east, west]
+        case "L":
+            return [north, east]
+        case "J":
+            return [north, west]
+        case "7":
+            return [south, west]
+        case "F":
+            return [south, east]
+        default:
+            return []
+        }
+    }
+    
+    func dijkstra2(target: Vector2D) -> [Vector2D: Int] {
+        var unvisited = Set<Vector2D>()
+        var visited = Set<Vector2D>()
+        var dist = [Vector2D: Int]()
+        
+        
+        unvisited.insert(target)
+        dist[target] = 0
+        var currentNode = target
+        while unvisited.isEmpty == false {
+            let neighbours = getNeighboursFor(currentNode)
+            for neighbour in neighbours {
+                if visited.contains(neighbour) == false {
+                    unvisited.insert(neighbour)
+                }
+                let alt = dist[currentNode]! + 1
+                if alt < dist[neighbour, default: Int.max] {
+                    dist[neighbour] = alt
+                }
+            }
+            
+            unvisited.remove(currentNode)
+            visited.insert(currentNode)
+            
+            if let newNode = unvisited.min(by: { dist[$0, default: Int.max] < dist[$1, default: Int.max] }) {
+                currentNode = newNode
+            }
+        }
+        return dist
+    }
+}
+
+// MARK: Part 1
+
+func getStepcount(_ input: String) -> Int {
+    let map = Map(input)
+    let dijkstra = map.dijkstra2(target: map.startingPosition)
+    let stepCount: Int = dijkstra.values.max()!
+    return stepCount
+}
+
+// MARK: Part 2
+extension Map {
+    init(nodes: [Vector2D: Character], colCount: Int, rowCount: Int, startingPosition: Vector2D) {
+        self.nodes = nodes
+        self.colCount = colCount
+        self.rowCount = rowCount
+        self.startingPosition = startingPosition
+    }
+    
+    func substitutePath() -> Map {
+        let path = dijkstra2(target: startingPosition)
+        var updatedNodes = nodes
+        
+        for tile in nodes.keys {
+            if path.keys.contains(tile) == false {
+                updatedNodes[tile] = "."
+            }
+        }
+        
+        return Map(nodes: updatedNodes, colCount: colCount, rowCount: rowCount, startingPosition: startingPosition)
+    }
+    
+    func findEnclosedTiles() -> [Vector2D] {
+         //simple flood fill
+         var toCheck = Set([Vector2D.zero])
+         var checked = Set<Vector2D>()
+        
+         let path = dijkstra2(target: startingPosition)
+        
+         while toCheck.isEmpty == false {
+             let currentCoord = toCheck.first!
+            
+             let neighbours = currentCoord.neighbours
+            
+             for neighbour in neighbours {
+                 if nodes.keys.contains(neighbour) && path.keys.contains(neighbour) == false && checked.contains(neighbour) == false {
+                     toCheck.insert(neighbour)
+                 }
+             }
+            
+             toCheck.remove(currentCoord)
+             checked.insert(currentCoord)
+         }
+        
+         // find all . in map
+        let allDots = Set(nodes.filter { $0.value == "." }.keys)
+         let notFilled = allDots.subtracting(checked)
+        
+         return Array(notFilled)
+    }
+    
+    func scale3() -> Map {
+        var updatedNodes = [Vector2D: Character]()
+                
+        for node in nodes {
+            updatedNodes[node.key * 3] = node.value
+
+            let east = Vector2D(x: node.key.x + 1, y: node.key.y)
+            let south = Vector2D(x: node.key.x, y: node.key.y + 1)
+            
+            // scale east
+            updatedNodes[Vector2D(x: 3 * node.key.x + 1, y: 3 * node.key.y)] =
+            getNeighboursFor(node.key).contains(east) && getNeighboursFor(east).contains(node.key) ? "-" : ","
+            updatedNodes[Vector2D(x: 3 * node.key.x + 2, y: 3 * node.key.y)] =
+            getNeighboursFor(node.key).contains(east) && getNeighboursFor(east).contains(node.key) ? "-" : ","
+                
+            // scale south
+            updatedNodes[Vector2D(x: 3 * node.key.x, y: 3 * node.key.y + 1)] = getNeighboursFor(node.key).contains(south) && getNeighboursFor(south).contains(node.key) ? "|" : ","
+            updatedNodes[Vector2D(x: 3 * node.key.x, y: 3 * node.key.y + 2)] = getNeighboursFor(node.key).contains(south) && getNeighboursFor(south).contains(node.key) ? "|" : ","
+                
+            // fill in the rest
+            updatedNodes[Vector2D(x: 3 * node.key.x + 1, y: 3 * node.key.y + 1)] = ","
+            updatedNodes[Vector2D(x: 3 * node.key.x + 2, y: 3 * node.key.y + 2)] = ","
+            updatedNodes[Vector2D(x: 3 * node.key.x + 1, y: 3 * node.key.y + 2)] = ","
+            updatedNodes[Vector2D(x: 3 * node.key.x + 2, y: 3 * node.key.y + 1)] = ","
+        }
+        
+        return Map(nodes: updatedNodes, colCount: colCount * 3, rowCount: rowCount * 3, startingPosition: startingPosition * 3)
+    }
+}
--- day10/Tests/day10Tests/day10Tests.swift
+++ day10/Tests/day10Tests/day10Tests.swift
@@ -2,11 +2,273 @@ import XCTest
 @testable import day10
 
 final class day10Tests: XCTestCase {
-    func testExample() throws {
-        // XCTest Documentation
-        // https://developer.apple.com/documentation/xctest
+    let exampleInput_simpleCase =
+    """
+    .....
+    .S-7.
+    .|.|.
+    .L-J.
+    .....
+    """
+    
+    let exampleInput_complexCase =
+    """
+    7-F7-
+    .FJ|7
+    SJLL7
+    |F--J
+    LJ.LJ
+    """
+    
+    func test_map_determineStartingPosition_withExampleInput() {
+        let simpleMap = Map(exampleInput_simpleCase)
+        XCTAssertEqual(simpleMap.startingPosition, Vector2D(x: 1, y: 1))
+        
+        let complexMap = Map(exampleInput_complexCase)
+        XCTAssertEqual(complexMap.startingPosition, Vector2D(x: 0, y: 2))
+    }
+    
+    func test_map_getCharacterAt_startingPosition() {
+        let simpleMap = Map(exampleInput_simpleCase)
+        XCTAssertEqual(simpleMap.getCharacterAt(Vector2D(x: 1, y: 1)), "F")
+        
+        let complexMap = Map(exampleInput_complexCase)
+        XCTAssertEqual(complexMap.getCharacterAt(Vector2D(x: 0, y: 2)), "F")
+    }
+    
+    func test_map_getCharacterAt_with_exampleInput_simpleCase() {
+        let expected: [Vector2D: Character] = [
+            Vector2D(x: 0, y: 0): ".",
+            Vector2D(x: 1, y: 2): "|",
+            Vector2D(x: 3, y: 3): "J",
+        ]
+        
+        let map = Map(exampleInput_simpleCase)
+        
+        for pair in expected {
+            XCTAssertEqual(map.getCharacterAt(pair.key), pair.value)
+        }
+    }
+    
+    func test_map_getCharacterAt_with_exampleInput_complexCase() {
+        let expected: [Vector2D: Character] = [
+            Vector2D(x: 0, y: 0): "7",
+            Vector2D(x: 1, y: 2): "J",
+            Vector2D(x: 3, y: 3): "-",
+        ]
+        
+        let map = Map(exampleInput_complexCase)
+        
+        for pair in expected {
+            XCTAssertEqual(map.getCharacterAt(pair.key), pair.value)
+        }
+    }
+    
+    // MARK: Part 1
+    func test_map_dijkstra_with_exampleInput_simpleCase() {
+        let map = Map(exampleInput_simpleCase)
+        let dijkstra = map.dijkstra2(target: map.startingPosition)
+        printDijkstra(tiles: dijkstra, colCount: map.colCount, rowCount: map.rowCount)
+        let expected =
+        """
+        .....
+        .012.
+        .1.3.
+        .234.
+        ....S
+        """
+        let expectedMap = Map(expected)
+        
+        for pair in expectedMap.nodes.filter({ $0.value.isNumber }) {
+            XCTAssertEqual(String(dijkstra[pair.key, default: -1]), String(pair.value))
+        }
+        
+    }
+    
+    func test_map_dijkstra_with_exampleInput_complexCase() {
+        let map = Map(exampleInput_complexCase)
+        let dijkstra = map.dijkstra2(target: map.startingPosition)
+        printDijkstra(tiles: dijkstra, colCount: map.colCount, rowCount: map.rowCount)
+        let expected =
+        """
+        ..45.
+        .236.
+        01.78
+        14567
+        23..S
+        """
+        let expectedMap = Map(expected)
+        
+        for pair in expectedMap.nodes.filter({ $0.value.isNumber }) {
+            XCTAssertEqual(String(dijkstra[pair.key, default: -1]), String(pair.value))
+        }
+        
+    }
+    
+    func test_getStepCount() {
+        XCTAssertEqual(getStepcount(exampleInput_simpleCase), 4)
+        XCTAssertEqual(getStepcount(exampleInput_complexCase), 8)
+    }
+    
+    func test_part1() {
+        let result = getStepcount(input)
+        XCTAssertEqual(result, 7093)
+    }
+    
+    // MARK: Part 2
+    let exampleInput_part2_1 =
+    """
+    ...........
+    .S-------7.
+    .|F-----7|.
+    .||.....||.
+    .||.....||.
+    .|L-7.F-J|.
+    .|..|.|..|.
+    .L--J.L--J.
+    ...........
+    """
+    
+    let exampleInput_part2_2 =
+    """
+    ..........
+    .S------7.
+    .|F----7|.
+    .||....||.
+    .||....||.
+    .|L-7F-J|.
+    .|..||..|.
+    .L--JL--J.
+    ..........
+    """
+    
+    let exampleInput_part2_3 =
+    """
+    ......................
+    ..F----7F7F7F7F-7.....
+    ..|F--7||||||||FJ.....
+    ..||.FJ||||||||L7.....
+    .FJL7L7LJLJ||LJ.L-7...
+    .L--J.L7...LJS7F-7L7..
+    .....F-J..F7FJ|L7L7L7.
+    .....L7.F7||L7|.L7L7|.
+    ......|FJLJ|FJ|F7|.LJ.
+    .....FJL-7.||.||||....
+    .....L---J.LJ.LJLJ....
+    ......................
+    """
+    
+    let exampleInput_part2_4 =
+    """
+    ......................
+    .FF7FSF7F7F7F7F7F---7.
+    .L|LJ||||||||||||F--J.
+    .FL-7LJLJ||||||LJL-77.
+    .F--JF--7||LJLJ7F7FJ-.
+    .L---JF-JLJ.||-FJLJJ7.
+    .|F|F-JF---7F7-L7L|7|.
+    .|FFJF7L7F-JF7|JL---7.
+    .7-L-JL7||F7|L7F-7F7|.
+    .L.L7LFJ|||||FJL7||LJ.
+    .L7JLJL-JLJLJL--JLJ.L.
+    ......................
+    """
+    
+    func test_map_findEnclosedTiles_with_exampleInput_part2_1() {
+        let map = Map(exampleInput_part2_1)
+        
+        let expected = [
+            Vector2D(x: 2, y: 6),
+            Vector2D(x: 3, y: 6),
+            Vector2D(x: 7, y: 6),
+            Vector2D(x: 8, y: 6)
+        ]
+        
+        let result = map.findEnclosedTiles()
+        for coord in expected {
+            XCTAssertTrue(result.contains(coord))
+        }        
+    }
+    
+    func test_scale3x() {
+        let map = Map(exampleInput_part2_2)
+        let map_3x = map.scale3()
+        printMap(map_3x)
+    }
+    
+    func test_map_findEnclosedTiles_with_exampleInput_part2_2() {
+        let map = Map(exampleInput_part2_2)
+        let substitutedMap = map.substitutePath()
+        let map3x = substitutedMap.scale3()
+        
+        let expected = [
+            Vector2D(x: 2, y: 6),
+            Vector2D(x: 3, y: 6),
+            Vector2D(x: 6, y: 6),
+            Vector2D(x: 7, y: 6)
+        ].map { $0 * 3 }
+        
+        let result = map3x.findEnclosedTiles()
+        XCTAssertEqual(result.count, expected.count)
+        print(result)
+        for coord in expected {
+            XCTAssertTrue(result.contains(coord))
+        }
+    }
+    
+    func test_map_findEnclosedTiles_with_exampleInput_part2_3() {
+        let map = Map(exampleInput_part2_3)
+        let map3x = map.scale3()
+        let result = map3x.findEnclosedTiles()
+        XCTAssertEqual(result.count, 8)
+    }
+    
+    func test_map_findEnclosedTiles_with_exampleInput_part2_4() {
+        let map = Map(exampleInput_part2_4)
+        let substitutedMap = map.substitutePath()
+        let map3x = substitutedMap.scale3()
+        let result = map3x.findEnclosedTiles()
+        XCTAssertEqual(result.count, 10)
+    }
+    
+    func test_part2() {
+        let map = Map(input)
+        let substitutedMap = map.substitutePath()
+        let map3x = substitutedMap.scale3()
+        let result = map3x.findEnclosedTiles()
+        XCTAssertEqual(result.count, 407)
+    }
+}
 
-        // Defining Test Cases and Test Methods
-        // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
+func printDijkstra(tiles: [Vector2D: Int], colCount: Int, rowCount: Int) {
+    for y in 0 ..< rowCount {
+        var row = "|"
+        for x in 0 ..< colCount {
+            let coord = Vector2D(x: x, y: y)
+            if let value = tiles[coord] {
+                if value > 100 {
+                    row += "##|"
+                } else {
+                    row += String(format: "%02d", value) + "|"
+                }
+            } else {
+                row += "  |"
+            }
+        }
+        print(row)
+    }
+}
+
+func printMap(_ map: Map) {
+    let rowCount = map.rowCount
+    let colCount = map.colCount
+    for y in 0 ..< rowCount {
+        var row = ""
+        for x in 0 ..< colCount {
+            let coord = Vector2D(x: x, y: y)
+            let tile = map.nodes[coord, default: " "]
+            row += String(tile)
+        }
+        print(row)
     }
 }
