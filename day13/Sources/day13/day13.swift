d65124b4ea19a6ddbac68e22ae1f8a7b8835bb6e
Maarten Engels <maartene@mac.com>
2023-12-13 18:24:06 +0100

    Day 13 part 2 done

--- day13/Sources/day13/day13.swift
+++ day13/Sources/day13/day13.swift
@@ -1,2 +1,185 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
+import Foundation
+
+struct MountainRange {
+    let matrix: [[Bool]]
+    
+    var cacheCompareColumns = [Vector2D: Bool]()
+    var cacheCompareRows = [Vector2D: Bool]()
+    
+    let width: Int
+    let height: Int
+    
+    init(_ inputString: String) {
+        matrix = inputString.split(separator: "\n").map { String($0) }
+                .map { line in
+                    line.map { $0 == "#" }
+                }
+        
+        width = matrix.first?.count ?? 0
+        height = matrix.count
+    }
+    
+    func compareColumn(_ col1: Int, _ col2: Int, flipLocation: Vector2D? = nil) -> Bool {
+        guard col1 >= 0 && col1 < width && col2 >= 0 && col2 < width else {
+            return true
+        }
+        
+        for y in 0 ..< matrix.count {
+            let value1 = flipLocation?.x == col1 && flipLocation?.y == y ? !matrix[y][col1] : matrix[y][col1]
+            let value2 = flipLocation?.x == col2 && flipLocation?.y == y ? !matrix[y][col2] : matrix[y][col2]
+            if value1 != value2 {
+                return false
+            }
+        }
+        return true
+    }
+
+    func isVerticalMirror(col: Int, flipLocation: Vector2D? = nil) -> Bool {
+        var result = true
+        for i in 0 ..< width {
+            let colToCheck1 = col - i
+            let colToCheck2 = col + 1 + i
+            //let compareResult =
+            //print("Checking columns: \(colToCheck1) \(colToCheck2)")
+            
+            result = compareColumn(colToCheck1, colToCheck2, flipLocation: flipLocation) && result
+        }
+        return result
+    }
+
+    /// returns `nil` if no vertical reflection is found
+    func findVerticalReflection(flipLocation: Vector2D? = nil, ignoreColumn: Int? = nil) -> Int? {
+        var reflectionAfterColumn: Int?
+        var col = 0
+        while reflectionAfterColumn == nil && col < width - 1 {
+            //print("trying column :\(col)")
+            if col != ignoreColumn, isVerticalMirror(col: col, flipLocation: flipLocation) {
+                reflectionAfterColumn = col
+            }
+            col += 1
+        }
+        return reflectionAfterColumn
+    }
+
+    func compareRow(_ row1: Int, _ row2: Int, flipLocation: Vector2D? = nil) -> Bool {
+        guard row1 >= 0 && row1 < height && row2 >= 0 && row2 < height else {
+            return true
+        }
+        
+        for x in 0 ..< width {
+            let value1 = flipLocation?.x == x && flipLocation?.y == row1 ? !matrix[row1][x] : matrix[row1][x]
+            let value2 = flipLocation?.x == x && flipLocation?.y == row2 ? !matrix[row2][x] : matrix[row2][x]
+            if value1 != value2 {
+                return false
+            }
+        }
+        return true
+    }
+
+    func isHorizontalMirror(row: Int, flipLocation: Vector2D? = nil) -> Bool {
+        var result = true
+        for i in 0 ..< height {
+            let rowToCheck1 = row - i
+            let rowToCheck2 = row + 1 + i
+            
+            result = compareRow(rowToCheck1, rowToCheck2, flipLocation: flipLocation) && result
+        }
+        return result
+    }
+
+    func findHorizontalReflection(flipLocation: Vector2D? = nil, ignoreRow: Int? = nil) -> Int? {
+        var reflectionAfterRow: Int?
+        var row = 0
+        while reflectionAfterRow == nil && row < height - 1 {
+            //print("trying row :\(row)")
+            if row != ignoreRow, isHorizontalMirror(row: row, flipLocation: flipLocation) {
+                reflectionAfterRow = row
+            }
+            row += 1
+        }
+        return reflectionAfterRow
+    }
+}
+
+func createMountainRangesFromInput(_ input: String) -> [MountainRange] {
+    var lines = input.split(separator: "\n", omittingEmptySubsequences: false).map { String($0) }
+    var boardStrings = [String]()
+    while lines.count > 0 {
+        //print(lines)
+        if let endOfBoardIndex = lines.firstIndex(of: "") {
+            
+            boardStrings.append(lines[0 ..< endOfBoardIndex].joined(separator: "\n"))
+            lines = Array(lines.dropFirst(endOfBoardIndex))
+            lines = Array(lines.dropFirst())
+        } else {
+            boardStrings.append(lines.joined(separator: "\n"))
+            lines = []
+        }
+        
+    }
+    
+    //print(boardStrings.count, boardStrings)
+    
+    return boardStrings.map { MountainRange($0) }
+}
+
+func calculateCheckSum(_ input: String) -> Int {
+    let boards = createMountainRangesFromInput(input)
+    
+    let verticalMirrors = boards.compactMap {
+        $0.findVerticalReflection()
+    }.map { ($0 + 1) * 1 }
+    
+    let horizontalMirrors = boards.compactMap {
+        $0.findHorizontalReflection()
+    }.map { ($0 + 1) * 100 }
+    
+    return verticalMirrors.reduce(0, +) + horizontalMirrors.reduce(0, +)
+}
+
+
+// MARK: Part 2
+extension MountainRange {
+    func findReflectionWithFlipping() -> (col: Int?, row: Int?) {
+        var col: Int?
+        var row: Int?
+        
+        let existingCol = findVerticalReflection()
+        let existingRow = findHorizontalReflection()
+        
+        for y in 0 ..< height {
+            for x in 0 ..< width {
+                if row == nil && col == nil, let r = findHorizontalReflection(flipLocation: Vector2D(x: x, y: y), ignoreRow: existingRow), r != existingRow {
+                    row = r
+                }
+                
+                if col == nil && row == nil, let c = findVerticalReflection(flipLocation: Vector2D(x: x, y: y), ignoreColumn: existingCol), c != existingCol {
+                    col = c
+                }
+            }
+        }
+        
+        //print(existingCol, existingRow, col, row)
+        return (col, row)
+    }
+}
+
+func calculateCheckSumWithFlipping(_ input: String) -> Int {
+    let boards = createMountainRangesFromInput(input)
+    
+    var checksums = [Int]()
+    for i in 0 ..< boards.count {
+        print("\(Double(i)/Double(boards.count) * 100)%")
+        let reflection = boards[i].findReflectionWithFlipping()
+        var checksum = 0
+        if let col = reflection.col {
+            checksum += (col + 1)
+        }
+        if let row = reflection.row {
+            checksum += (row + 1) * 100
+        }
+        checksums.append(checksum)
+    }
+        
+    return checksums.reduce(0, +)
+}
--- day13/Tests/day13Tests/day13Tests.swift
+++ day13/Tests/day13Tests/day13Tests.swift
@@ -1,133 +1,6 @@
 import XCTest
 @testable import day13
 
-func compareColumn(_ col1: Int, _ col2: Int, in matrix: [[Bool]]) -> Bool {
-    let width = matrix.first?.count ?? 0
-    guard col1 >= 0 && col1 < width && col2 >= 0 && col2 < width else {
-        return true
-    }
-    
-    for y in 0 ..< matrix.count {
-        if matrix[y][col1] != matrix[y][col2] {
-            return false
-        }
-    }
-    return true
-}
-
-func isVerticalMirror(col: Int, in matrix: [[Bool]]) -> Bool {
-    let width = matrix.first?.count ?? 0
-        
-    var result = true
-    for i in 0 ..< width {
-        let colToCheck1 = col - i
-        let colToCheck2 = col + 1 + i
-        //let compareResult =
-        //print("Checking columns: \(colToCheck1) \(colToCheck2)")
-        
-        result = compareColumn(colToCheck1, colToCheck2, in: matrix) && result
-    }
-    return result
-}
-
-/// returns `nil` if no vertical reflection is found
-func findVerticalReflection(_ matrix: [[Bool]]) -> Int? {
-    var reflectionAfterColumn: Int?
-    let width = matrix.first?.count ?? 0
-    var col = 0
-    while reflectionAfterColumn == nil && col < width - 1 {
-        //print("trying column :\(col)")
-        if isVerticalMirror(col: col, in: matrix) {
-            reflectionAfterColumn = col
-        }
-        col += 1
-    }
-    return reflectionAfterColumn
-}
-
-func compareRow(_ row1: Int, _ row2: Int, in matrix: [[Bool]]) -> Bool {
-    let height = matrix.count
-    guard row1 >= 0 && row1 < height && row2 >= 0 && row2 < height else {
-        return true
-    }
-    
-    for x in 0 ..< matrix[0].count {
-        if matrix[row1][x] != matrix[row2][x] {
-            return false
-        }
-    }
-    return true
-}
-
-func isHorizontalMirror(row: Int, in matrix: [[Bool]]) -> Bool {
-    let height = matrix.count
-        
-    var result = true
-    for i in 0 ..< height {
-        let rowToCheck1 = row - i
-        let rowToCheck2 = row + 1 + i
-        //let compareResult =
-        //print("Checking columns: \(rowToCheck1) \(rowToCheck2)")
-        
-        result = compareRow(rowToCheck1, rowToCheck2, in: matrix) && result
-    }
-    return result
-}
-
-
-func findHorizontalReflection(_ matrix: [[Bool]]) -> Int? {
-    var reflectionAfterRow: Int?
-    let height = matrix.count
-    var row = 0
-    while reflectionAfterRow == nil && row < height - 1 {
-        //print("trying row :\(row)")
-        if isHorizontalMirror(row: row, in: matrix) {
-            reflectionAfterRow = row
-        }
-        row += 1
-    }
-    return reflectionAfterRow
-}
-
-func calculateCheckSum(_ input: String) -> Int {
-    var lines = input.split(separator: "\n", omittingEmptySubsequences: false).map { String($0) }
-    var boardStrings = [String]()
-    var i = 0
-    while lines.count > 0 {
-        //print(lines)
-        if let endOfBoardIndex = lines.firstIndex(of: "") {
-            
-            boardStrings.append(lines[0 ..< endOfBoardIndex].joined(separator: "\n"))
-            lines = Array(lines.dropFirst(endOfBoardIndex))
-            lines = Array(lines.dropFirst())
-        } else {
-            boardStrings.append(lines.joined(separator: "\n"))
-            lines = []
-        }
-        
-    }
-    
-    //print(boardStrings.count, boardStrings)
-    
-    let boards = boardStrings.map { boardString in
-        boardString.split(separator: "\n").map { String($0) }
-            .map { line in
-                line.map { $0 == "#" }
-            }
-    }
-    
-    let verticalMirrors = boards.compactMap {
-        findVerticalReflection($0)
-    }.map { ($0 + 1) * 1 }
-    
-    let horizontalMirrors = boards.compactMap {
-        findHorizontalReflection($0)
-    }.map { ($0 + 1) * 100 }
-    
-    return verticalMirrors.reduce(0, +) + horizontalMirrors.reduce(0, +)
-}
-
-
 final class day13Tests: XCTestCase {
     let exampleInput1 =
     """
@@ -170,48 +43,42 @@ final class day13Tests: XCTestCase {
     #....#..#
     """
     
-    lazy var matrixExample1 = exampleInput1.split(separator: "\n").map { String($0) }
-        .map { line in
-            line.map { $0 == "#" }
-        }
+    lazy var mountainRangeExample1 = MountainRange(exampleInput1)
     
-    lazy var matrixExample2 = exampleInput2.split(separator: "\n").map { String($0) }
-        .map { line in
-            line.map { $0 == "#" }
-        }
+    lazy var mountainRangeExample2 = MountainRange(exampleInput2)
     
     func test_isVerticalReflection_withExampleInput_with5_returnsTrue() {
-        let result = isVerticalMirror(col: 4, in: matrixExample1)
+        let result = mountainRangeExample1.isVerticalMirror(col: 4)
         XCTAssertTrue(result)
     }
     
     func test_isVerticalReflection_withExampleInput_with6_returnsFalse() {
-        let result = isVerticalMirror(col: 5, in: matrixExample1)
+        let result = mountainRangeExample1.isVerticalMirror(col: 5)
         XCTAssertFalse(result)
     }
     
     func test_isVerticalReflection_withExampleInput_with0_returnsFalse() {
-        let result = isVerticalMirror(col: 0, in: matrixExample1)
+        let result = mountainRangeExample1.isVerticalMirror(col: 0)
         XCTAssertFalse(result)
     }
     
     func test_findVerticalReflection_withExampleInput1() {
-        let result = findVerticalReflection(matrixExample1)
+        let result = mountainRangeExample1.findVerticalReflection()
         XCTAssertEqual(result, 4)
     }
     
     func test_findVerticalReflection_withExampleInput2_returnsNil() {
-        let result = findVerticalReflection(matrixExample2)
+        let result = mountainRangeExample2.findVerticalReflection()
         XCTAssertNil(result)
     }
     
     func test_findHorizontalReflection_withExampleInput2() {
-        let result = findHorizontalReflection(matrixExample2)
+        let result = mountainRangeExample2.findHorizontalReflection()
         XCTAssertEqual(result, 3)
     }
     
     func test_findHorizontalReflection_withExampleInput1_returnsNil() {
-        let result = findHorizontalReflection(matrixExample1)
+        let result = mountainRangeExample1.findHorizontalReflection()
         XCTAssertNil(result)
     }
     
@@ -223,5 +90,65 @@ final class day13Tests: XCTestCase {
     func test_part1() {
         let result = calculateCheckSum(input)
         XCTAssertEqual(result, 30802)
+    }
+    
+    // MARK: Part 2
+    func test_findHorizontalReflection_withExampleInput1_returns2_whenFlipping0_0() {
+        let result = mountainRangeExample1.findHorizontalReflection(flipLocation: Vector2D(x: 0, y: 0))
+        XCTAssertEqual(result, 2)
+    }
+    
+    func test_findHorizontalReflection_withExampleInput2_returns0_whenFlipping4_1() {
+        let result = mountainRangeExample2.findHorizontalReflection(flipLocation: Vector2D(x: 4, y: 1))
+        XCTAssertEqual(result, 0)
+    }
+    
+    func test_findHorizontalReflectionWithFlipping_withExampleInput1_returns2() {
+        let result = mountainRangeExample1.findReflectionWithFlipping()
+        XCTAssertEqual(result.col, nil)
+        XCTAssertEqual(result.row, 2)
+    }
+    
+    func test_findHorizontalReflectionWithFlipping_withExampleInput2_returns0() {
+        let result = mountainRangeExample2.findReflectionWithFlipping()
+        XCTAssertEqual(result.col, nil)
+        XCTAssertEqual(result.row, 0)
+    }
+    
+    func test_calculateCheckSumWithFlipping_withExampleInput() {
+        let result = calculateCheckSumWithFlipping(exampleInput)
+        XCTAssertEqual(result, 400)
+    }
+    
+    func test_part2() {
+        let result = calculateCheckSumWithFlipping(input)
+        XCTAssertEqual(result, 37876)
+    }
+    
+    let exampleInput3 =
+    """
+    ..#.#...##.
+    ##.#.##.##.
+    ##.##.##..#
+    ..#..##.##.
+    ##.#.###.##
+    ..#.#..#..#
+    ..###..#..#
+    ..###.##..#
+    ##....#####
+    ##..##.....
+    ..#..#.#..#
+    """
+    
+    func test_calculateChecksum_withExampleInput3() {
+        let result = calculateCheckSum(exampleInput3)
+        XCTAssertEqual(result, 1)
+    }
+    
+    func test_calculateChecksumWithFlipping_withExampleInput3() {
+        let exampleRange3 = MountainRange(exampleInput3)
+        let result = exampleRange3.findReflectionWithFlipping()
+        XCTAssertEqual(result.col, 8)
+        XCTAssertEqual(result.row, nil)
     }
 }
