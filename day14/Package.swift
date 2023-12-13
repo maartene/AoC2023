e13799f32afe8ed14633d881aaa31d2d788caa16
Maarten Engels <maartene@mac.com>
2023-12-13 19:18:51 +0100

    Day 14 scaffold

---
+++ day14/Package.swift
@@ -0,0 +1,23 @@
+// swift-tools-version: 5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "day14",
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "day14",
+            targets: ["day14"]),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "day14"),
+        .testTarget(
+            name: "day14Tests",
+            dependencies: ["day14"]),
+    ]
+)
