abff626912794db7922c8b4a241136adc60f1e01
Maarten Engels <maartene@thedreamweb.eu>
2023-12-16 12:09:52 +0100

    Day 16 done

--- day16/Sources/day16/day16.swift
+++ day16/Sources/day16/day16.swift
@@ -1,2 +1,185 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
+import Foundation
+
+enum Direction {
+    case left
+    case up
+    case right
+    case down
+}
+
+struct Beam {
+    let x: Int
+    let y: Int
+    let direction: Direction
+    
+    init(_ x: Int, _ y: Int, _ direction: Direction) {
+        self.x = x
+        self.y = y
+        self.direction = direction
+    }
+}
+
+extension Beam: Equatable { }
+extension Beam: Hashable { }
+
+class Contraption {
+    let matrix: [[Character]]
+    let height: Int
+    let width: Int
+    
+    init(_ inputString: String) {
+        matrix = inputString.split(separator: "\n").map { String($0)
+                .map { $0 }
+        }
+            
+        height = matrix.count
+        width = matrix.first?.count ?? 0
+    }
+    
+    func nextPositionFor(beam: Beam) -> [Beam] {
+        let character = matrix[beam.y][beam.x]
+        switch character {
+        case ".":
+            return [processDot(for: beam)]
+        case "|":
+            return processVerticalBar(for: beam)
+        case "-":
+            return processHorizontalBar(for: beam)
+        case "/":
+            return [processForwardSlash(for: beam)]
+        case "\\":
+            return [processBackwardSlash(for: beam)]
+        default:
+            fatalError("Unexpected token: \(character)")
+        }
+        
+    }
+    
+    func processDot(for beam: Beam) -> Beam {
+        switch beam.direction {
+        case .right:
+            return beam.x < width - 1 ? Beam(beam.x + 1, beam.y, beam.direction) : beam
+        case .down:
+            return beam.y < height - 1 ? Beam(beam.x, beam.y + 1, beam.direction) : beam
+        case .up:
+            return beam.y > 0 ? Beam(beam.x, beam.y - 1, beam.direction) : beam
+        case .left:
+            return beam.x > 0 ? Beam(beam.x - 1, beam.y, beam.direction) : beam
+        }
+    }
+    
+    func processVerticalBar(for beam: Beam) -> [Beam] {
+        var result = [Beam]()
+        
+        guard [.left, .right].contains(beam.direction) else {
+            return [processDot(for: beam)]
+        }
+        
+        result.append(Beam(beam.x, beam.y - 1, .up))
+        result.append(Beam(beam.x, beam.y + 1, .down))
+        
+        return result.filter {
+            $0.x >= 0 && $0.x < width && $0.y >= 0 && $0.y < height
+        }
+    }
+    
+    func processHorizontalBar(for beam: Beam) -> [Beam] {
+        var result = [Beam]()
+        
+        guard [.up, .down].contains(beam.direction) else {
+            return [processDot(for: beam)]
+        }
+        
+        result.append(Beam(beam.x - 1, beam.y, .left))
+        result.append(Beam(beam.x + 1, beam.y, .right))
+        
+        return result.filter {
+            $0.x >= 0 && $0.x < width && $0.y >= 0 && $0.y < height
+        }
+    }
+    
+    func processForwardSlash(for beam: Beam) -> Beam {
+        let result: Beam
+        switch beam.direction {
+        case .right:
+            result = Beam(beam.x, beam.y - 1, .up)
+        case .down:
+            result = Beam(beam.x - 1, beam.y, .left)
+        case .up:
+            result = Beam(beam.x + 1, beam.y, .right)
+        case .left:
+            result = Beam(beam.x, beam.y + 1, .down)
+        }
+        
+        if result.x >= 0 && result.x < width && result.y >= 0 && result.y < height {
+            return result
+        } else {
+            return beam
+        }
+    }
+    
+    func processBackwardSlash(for beam: Beam) -> Beam {
+        let result: Beam
+        
+        switch beam.direction {
+        case .right:
+            result = Beam(beam.x, beam.y + 1, .down)
+        case .down:
+            result = Beam(beam.x + 1, beam.y, .right)
+        case .up:
+            result = Beam(beam.x - 1, beam.y, .left)
+        case .left:
+            result = Beam(beam.x, beam.y - 1, .up)
+        }
+        
+        if result.x >= 0 && result.x < width && result.y >= 0 && result.y < height {
+            return result
+        } else {
+            return beam
+        }
+    }
+    
+    func energizedTilesCount(startBeam: Beam) -> Int {
+        struct Coord: Hashable {
+            let x: Int
+            let y: Int
+        }
+        
+        var beams = Set([startBeam])
+        var energizedTiles = Set<Beam>()
+        
+        while beams.isEmpty == false {
+            let beam = beams.removeFirst()
+            energizedTiles.insert(beam)
+            for newBeam in nextPositionFor(beam: beam) {
+                if energizedTiles.contains(newBeam) == false { beams.insert(newBeam) }
+            }
+        }
+        
+        let coords = energizedTiles.reduce(into: Set<Coord>()) { $0.insert(Coord(x: $1.x, y: $1.y)) }
+        return coords.count
+    }
+    
+    func calculateMaximumEnergizedCount() -> Int {
+        var startBeams = [Beam]()
+        for x in 0 ..< width {
+            startBeams.append(Beam(x, 0, .down))
+            startBeams.append(Beam(x, height - 1, .up))
+        }
+        
+        for y in 0 ..< height {
+            startBeams.append(Beam(0, y, .right))
+            startBeams.append(Beam(width - 1, y, .left))
+        }
+        
+        var maximum = 0
+        for startBeam in startBeams {
+            let energizedCount = energizedTilesCount(startBeam: startBeam)
+            if energizedCount > maximum {
+                maximum = energizedCount
+            }
+        }
+        
+        return maximum
+    }
+}
--- day16/Tests/day16Tests/day16Tests.swift
+++ day16/Tests/day16Tests/day16Tests.swift
@@ -2,11 +2,208 @@ import XCTest
 @testable import day16
 
 final class day16Tests: XCTestCase {
-    func testExample() throws {
-        // XCTest Documentation
-        // https://developer.apple.com/documentation/xctest
-
-        // Defining Test Cases and Test Methods
-        // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
+    let exampleInput =
+    #"""
+    .|...\....
+    |.-.\.....
+    .....|-...
+    ........|.
+    ..........
+    .........\
+    ..../.\\..
+    .-.-/..|..
+    .|....-|.\
+    ..//.|....
+    """#
+    
+    lazy var contraption = Contraption(exampleInput)
+    
+    func test_beamContinuesInDirection() {
+        let startPositions = [
+            Beam(0,0, Direction.right),
+            Beam(0,2, Direction.down),
+            Beam(1,3, Direction.up),
+            Beam(1,3, Direction.left)
+            ]
+        
+        let expected = [
+            [Beam(1, 0, Direction.right)],
+            [Beam(0, 3, Direction.down)],
+            [Beam(1, 2, Direction.up)],
+            [Beam(0, 3, Direction.left)]
+            ]
+            
+        for i in 0 ..< expected.count {
+            XCTAssertEqual(contraption.nextPositionFor(beam: startPositions[i]), expected[i])
+        }
+        
+    }
+    
+    func test_beamCannotContinueInDirection_AtBorder() {
+        let startPositions = [
+            Beam(9,0, Direction.right),
+            Beam(8,9, Direction.down),
+            Beam(3,0, Direction.up),
+            Beam(0,3, Direction.left)
+            ]
+        
+        for position in startPositions {
+            XCTAssertEqual(contraption.nextPositionFor(beam: position)[0], position)
+        }
+    }
+        
+    func test_beamSplitsAtSplitter_whenHittingFlatSide() {
+        let startPositions = [
+            Beam(5, 2, Direction.right),
+            Beam(5, 2, Direction.left),
+            ]
+        
+        let expected = [
+            [Beam(5, 1, Direction.up), Beam(5, 3, Direction.down)],
+            [Beam(5, 1, Direction.up), Beam(5, 3, Direction.down)]
+        ]
+        
+        for (i, startPosition) in startPositions.enumerated() {
+            let result = contraption.nextPositionFor(beam: startPosition)
+            XCTAssertEqual(result, expected[i])
+        }
+    }
+    
+    func test_beamDoesntSplitAtSplitter_whenHittingPointySide() {
+        let startPositions = [
+            Beam(5, 2, Direction.down),
+            Beam(5, 2, Direction.up),
+            ]
+        
+        let expected = [
+            [Beam(5, 3, Direction.down)],
+            [Beam(5, 1, Direction.up)]
+        ]
+        
+        for (i, startPosition) in startPositions.enumerated() {
+            let result = contraption.nextPositionFor(beam: startPosition)
+            XCTAssertEqual(result, expected[i])
+        }
+    }
+    
+    func test_beamSplit_doesntReturnOutOfBoundsBeams() {
+        let startPositions = [
+            Beam(1, 0, Direction.left),
+            Beam(1, 0, Direction.right),
+            ]
+        
+        let expected = [
+            [Beam(1, 1, Direction.down)],
+            [Beam(1, 1, Direction.down)],
+        ]
+        
+        for (i, startPosition) in startPositions.enumerated() {
+            let result = contraption.nextPositionFor(beam: startPosition)
+            XCTAssertEqual(result, expected[i])
+        }
+    }
+    
+    func test_beamSplitsAtDashSplitter_whenHittingFlatSide() {
+        let startPositions = [
+            Beam(6, 2, Direction.up),
+            Beam(6, 2, Direction.down),
+            ]
+        
+        let expected = [
+            [Beam(5, 2, Direction.left), Beam(7, 2, Direction.right)],
+            [Beam(5, 2, Direction.left), Beam(7, 2, Direction.right)]
+        ]
+        
+        for (i, startPosition) in startPositions.enumerated() {
+            let result = contraption.nextPositionFor(beam: startPosition)
+            XCTAssertEqual(result, expected[i])
+        }
+    }
+    
+    func test_beamDoesntSplitAtDashSplitter_whenHittingPointySide() {
+        let startPositions = [
+            Beam(6, 2, Direction.left),
+            Beam(6, 2, Direction.right),
+            ]
+        
+        let expected = [
+            [Beam(5, 2, Direction.left)],
+            [Beam(7, 2, Direction.right)],
+        ]
+        
+        for (i, startPosition) in startPositions.enumerated() {
+            let result = contraption.nextPositionFor(beam: startPosition)
+            XCTAssertEqual(result, expected[i])
+        }
+    }
+    
+    // If the beam encounters a mirror (/ or \), the beam is reflected 90 degrees depending on the angle of the mirror. For instance, a rightward-moving beam that encounters a / mirror would continue upward in the mirror's column, while a rightward-moving beam that encounters a \ mirror would continue downward from the mirror's column.
+    func test_forwardSlashMirror() {
+        // / mirror
+        let startPositions = [
+            Beam(4, 6, Direction.right),
+            Beam(4, 6, Direction.down),
+            Beam(4, 6, Direction.up),
+            Beam(4, 6, Direction.left),
+            ]
+        
+        let expected = [
+            [Beam(4, 5, Direction.up)],
+            [Beam(3, 6, Direction.left)],
+            [Beam(5, 6, Direction.right)],
+            [Beam(4, 7, Direction.down)],
+            
+        ]
+        
+        for (i, startPosition) in startPositions.enumerated() {
+            let result = contraption.nextPositionFor(beam: startPosition)
+            XCTAssertEqual(result, expected[i])
+        }
+    }
+    
+    func test_backSlashMirror() {
+        // / mirror
+        let startPositions = [
+            Beam(4, 1, Direction.right),
+            Beam(4, 1, Direction.down),
+            Beam(4, 1, Direction.up),
+            Beam(4, 1, Direction.left),
+            ]
+        
+        let expected = [
+            [Beam(4, 2, Direction.down)],
+            [Beam(5, 1, Direction.right)],
+            [Beam(3, 1, Direction.left)],
+            [Beam(4, 0, Direction.up)],
+            
+        ]
+        
+        for (i, startPosition) in startPositions.enumerated() {
+            let result = contraption.nextPositionFor(beam: startPosition)
+            XCTAssertEqual(result, expected[i])
+        }
+    }
+    
+    func test_energizedTilesCount_withExampleInput() {
+        let result = contraption.energizedTilesCount(startBeam: Beam(0, 0, .right))
+        XCTAssertEqual(result, 46)
+    }
+    
+    func test_part1() {
+        let contraption = Contraption(input)
+        let result = contraption.energizedTilesCount(startBeam: Beam(0, 0, .right))
+        XCTAssertEqual(result, 7798)
+    }
+    
+    // MARK: Part 2
+    func test_maximumEnergizedCount() {
+        let result = contraption.calculateMaximumEnergizedCount()
+        XCTAssertEqual(result, 51)
+    }
+    
+    func test_part2() {
+        let contraption = Contraption(input)
+        let result = contraption.calculateMaximumEnergizedCount()
+        XCTAssertEqual(result, 8026)
     }
 }
